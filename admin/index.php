48049cb4217b8e285ef9d55987b7ae2134e62578
Harshit Harchani <hharchani@gmail.com>
2014-08-28 04:12:00 +0530

    Added allow button

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,6 @@
 <?php
 	//Admin panel
-	
+	require_once("../config.php");
+
+	$htmlOutput = "<form action='allow.php' method='post'><input type='hidden' name='allowed' value='true'>
+			<input type='submit' value='Allow 1 Vote'>";
