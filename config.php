6f92d0aaa4e6c96ebfd3ba45145f6f7d96b8e971
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-08-28 03:01:50 +0530

    Added template, updated config

--- config.php
+++ config.php
@@ -4,6 +4,8 @@
 	define('DB_PASS', 'cre12321');
 	define('DB_NAME', 'CRE');
 	
+	$base_url = 'http://localhost/CRElection/';
+	
 	// Database connection
 	$DB = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 	if (mysqli_connect_errno()){
---
+++ template.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Election Portal</title>
+		<link href='<?php echo $base_url; ?>css/normalize.css' rel='stylesheet'>
+		<link href='<?php echo $base_url; ?>css/main.css' rel='stylesheet'>
+	</head>
+	<body>
+	</body>
+</html>
