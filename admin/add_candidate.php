9ee571af1008260a4b4b11c78dacf3ae1d75f104
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-11-11 20:21:05 +0530

    Fix in `add_candidate.php`

--- admin/add_candidate.php
+++ admin/add_candidate.php
@@ -2,27 +2,28 @@
 	//Add Candidate
 	$auth_required = true;
 	require_once("../config.php");
-        
-        if ( isset($_POST["candidate_name"]) ) {
-            $name = $_POST["candidate_name"];
-	    if ( strpos($name, 'remove:') !== 0 ) {
-		$query = mysqli_prepare($DB, "INSERT INTO `candidates` (name) VALUES (?)");
-		mysqli_stmt_bind_param($query, 's', $_POST["candidate_name"]);
-		if ( !mysqli_stmt_execute($query) ) { 
-			die("Some error occured. Coundn't remove candidate. Contact Administrator.");
+
+	if ( isset($_POST["candidate_name"]) ) {
+		$name = $_POST["candidate_name"];
+		if ( strpos($name, 'remove:') !== 0 ) {
+			$query = mysqli_prepare($DB, "INSERT INTO `candidates` (name) VALUES (?)");
+			mysqli_stmt_bind_param($query, 's', $_POST["candidate_name"]);
+			if ( !mysqli_stmt_execute($query) ) {
+				die("Some error occured. Coundn't remove candidate. Contact Administrator.");
+			}
 		}
-	    }
-	    else {
-		$name = str_replace('remove:','',$name);
-		$name = trim($name);
-		$query = mysqli_prepare($DB, "DELETE FROM `candidates` WHERE name = '$name'");
-		if ( !mysqli_stmt_execute($query) ) { 
-			die("Some Error Occured. Coundn't add candidate. Contact Administrator.");
+		else {
+			$name = str_replace('remove:','',$name);
+			$name = trim($name);
+			$query = mysqli_prepare($DB, "DELETE FROM `candidates` WHERE name = ?");
+			mysqli_stmt_bind_param($query, 's', $name);
+			if ( !mysqli_stmt_execute($query) ) {
+				die("Some Error Occured. Coundn't add candidate. Contact Administrator.");
+			}
 		}
-	    }
-        }
-        else {
-            die("Name Can't be empty");
-        }
-        
-        header("Location: ".$base_url."admin/candidates.php");
+	}
+	else {
+		die("Name Can't be empty");
+	}
+
+	header("Location: ".$base_url."admin/candidates.php");
