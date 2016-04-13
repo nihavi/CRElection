3ed4dfd80cb3b1e7012c479a03f9fdcf59f9194b
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-04-14 04:31:49 +0530

    Remove get_candidates() function from candidates.php

--- admin/candidates.php
+++ admin/candidates.php
@@ -1,22 +1,9 @@
 <?php
 	$auth_required = true;
 	require_once("../config.php");
-	
-	function get_candidates() {
-		global $DB;
-		$query = mysqli_prepare($DB, "SELECT id, name FROM `candidates`");
-		mysqli_stmt_execute($query);
-		mysqli_stmt_bind_result($query, $id, $name);
-		mysqli_stmt_store_result($query);
-		$results = array();
-		while(mysqli_stmt_fetch($query)){
-			$results[$id] = $name;
-		}
-		return $results;
-	}
-	
+
 	$htmlOutput = "";
-	
+
 	$htmlOutput .= "<h1>Candidates</h1>";
 	$candidates = get_candidates();
 	if ( count($candidates) != 0 ) {
@@ -29,15 +16,15 @@
 	else {
 		$htmlOutput .= "None<br>";
 	}
-	
+
 	$htmlOutput .= "<br><hr><br>";
 	$htmlOutput .= "<h3>Add a candidate</h3>";
 	$htmlOutput .= "<form action='add_candidate.php' method='post' autocomplete='off'>
 			Name <input type='text' name='candidate_name' required><br>
 			<input class='btn' type='submit' value='Add Candidate'>";
-	
+
 	$htmlOutput .= "<script>document.body.onload=function(){document.getElementsByName('candidate_name')[0].focus()}</script>";
-	
+
 	$htmlOutput .= "<br><br><br><br><a class='btn btn-green' href='".$base_url."admin/'>Go to Allow Voting</a>";
-	
+
 	include("../template.php");
