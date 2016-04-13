8be8217af0a080d9d23f8fbc402205669042eb37
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-04-14 04:02:02 +0530

    Add prepare_votes.php, to prefill DB with empty votes

---
+++ admin/prepare_votes.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../config.php';
+
+if (php_sapi_name() !== 'cli') {
+    exit("This script must be run from the command line.");
+}
+
+if (count($argv) < 2) {
+    exit("usage: prepare.php <electorate size>\n");
+}
+
+$n = $argv[1];
+
+$values_string = implode(",", array_fill(0, $n, "()"));
+
+if (mysqli_query($DB, "INSERT INTO `votes` VALUES " . $values_string)) {
+    echo "Successfully inserted $n empty rows into 'votes' table.\n";
+}
