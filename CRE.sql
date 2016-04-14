62089dea03a076cdb18ab3e282099a7f7aafea06
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2016-04-14 19:42:51 +0530

    Add votes table to CRE.sql

--- CRE.sql
+++ CRE.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.0-dev
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 11, 2014 at 08:43 PM
--- Server version: 5.5.40-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.5
+-- Generation Time: Apr 14, 2016 at 07:41 PM
+-- Server version: 5.5.47-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `CRE`
@@ -26,12 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `candidates`
 --
 
-CREATE TABLE IF NOT EXISTS `candidates` (
-`id` int(11) NOT NULL,
+CREATE TABLE `candidates` (
+  `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `votes` int(11) NOT NULL,
   `n_votes` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -39,12 +39,12 @@ CREATE TABLE IF NOT EXISTS `candidates` (
 -- Table structure for table `clients`
 --
 
-CREATE TABLE IF NOT EXISTS `clients` (
-`id` int(11) NOT NULL,
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
   `name` tinytext NOT NULL,
   `ip` varchar(15) NOT NULL,
   `allow_vote` tinyint(1) NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -52,11 +52,11 @@ CREATE TABLE IF NOT EXISTS `clients` (
 -- Table structure for table `meta`
 --
 
-CREATE TABLE IF NOT EXISTS `meta` (
-`id` int(11) NOT NULL,
+CREATE TABLE `meta` (
+  `id` int(11) NOT NULL,
   `meta_name` text NOT NULL,
   `meta_value` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `meta`
@@ -64,6 +64,17 @@ CREATE TABLE IF NOT EXISTS `meta` (
 
 INSERT INTO `meta` (`id`, `meta_name`, `meta_value`) VALUES
 (1, 'admin_pass', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL,
+  `vote_string` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -73,19 +84,25 @@ INSERT INTO `meta` (`id`, `meta_name`, `meta_value`) VALUES
 -- Indexes for table `candidates`
 --
 ALTER TABLE `candidates`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `clients`
 --
 ALTER TABLE `clients`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `meta`
 --
 ALTER TABLE `meta`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -95,17 +112,22 @@ ALTER TABLE `meta`
 -- AUTO_INCREMENT for table `candidates`
 --
 ALTER TABLE `candidates`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `meta`
 --
 ALTER TABLE `meta`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
