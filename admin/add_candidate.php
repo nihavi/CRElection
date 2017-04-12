4fa9a43ab3167a825d2c12b7c2f784b973e4c1b4
Harshit Harchani <hharchani@users.noreply.github.com>
2017-04-12 17:08:19 +0530

    Correct error messages for add/remove candidates

--- admin/add_candidate.php
+++ admin/add_candidate.php
@@ -5,20 +5,20 @@
 
 	if ( isset($_POST["candidate_name"]) ) {
 		$name = $_POST["candidate_name"];
-		if ( strpos($name, 'remove:') !== 0 ) {
+		if ( strpos($name, 'remove:') !== 0 ) { // Trying to add candidate
 			$query = mysqli_prepare($DB, "INSERT INTO `candidates` (name) VALUES (?)");
 			mysqli_stmt_bind_param($query, 's', $_POST["candidate_name"]);
 			if ( !mysqli_stmt_execute($query) ) {
-				die("Some error occured. Couldn't remove candidate. Contact Administrator.");
+				die("Some error occured. Couldn't add candidate. Contact Administrator.");
 			}
 		}
-		else {
+		else { // Trying to remove candidate.
 			$name = str_replace('remove:','',$name);
 			$name = trim($name);
 			$query = mysqli_prepare($DB, "DELETE FROM `candidates` WHERE name = ?");
 			mysqli_stmt_bind_param($query, 's', $name);
 			if ( !mysqli_stmt_execute($query) ) {
-				die("Some error occurred. Couldn't add candidate. Contact Administrator.");
+				die("Some error occurred. Couldn't remove candidate. Contact Administrator.");
 			}
 		}
 	}
